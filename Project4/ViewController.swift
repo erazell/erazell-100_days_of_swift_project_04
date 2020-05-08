8d426b9e73825a7fd203b9e33884ae0d7d557388
Janusz <janklyz@gmail.com>
2020-05-08 17:10:24 +0200

    add WKWebView instance

---
+++ Project4/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Janusz  on 08/05/2020.
+//  Copyright © 2020 Janusz . All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://www.apple.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+    }
+
+
+}
+
